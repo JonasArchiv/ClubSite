457d9076d4a54700e637b9e1dd3d99686bf77440
Jonas Heilig <jonas.maximilian@die-heiligs.de>
2024-07-01 19:41:25 +0200

    login, logout, default_user

---
+++ app.py
@@ -0,0 +1,66 @@
+import os
+from datetime import datetime
+from flask import Flask, render_template, request, redirect, session, url_for
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash, generate_password_hash
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.secret_key = 'secret_salt'  # Change this on Public build!
+db = SQLAlchemy(app)
+
+app_name = 'ClubSite'
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    leader = db.Column(db.Boolean, default=False)
+    author = db.Column(db.Boolean, default=False)
+
+
+def create_default_user():
+    admin_user = User(username='admin', password=generate_password_hash('password'), leader=True, author=True)
+    db.session.add(admin_user)
+    db.session.commit()
+
+
+if not os.path.exists('instance/db.db'):
+    with app.app_context():
+        db.create_all()
+        create_default_user()
+        print("Datenbank erstellt.")
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', app_name=app_name)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if 'username' in session:
+        return redirect(url_for('index'))
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user_in_session = User.query.filter_by(username=username).first()
+        if user_in_session and check_password_hash(user_in_session.password, password):
+            session['username'] = user_in_session.username
+            return redirect(url_for('index'))
+        else:
+            return render_template(login.html, app_name=app_name, error='Username or Password is wrong.')
+    return render_template('login.html', app_name=app_name)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
