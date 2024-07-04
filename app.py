fd23a0abe42d62ceb8ebba648675c3128e599af5
Jonas Heilig <jonas.maximilian@die-heiligs.de>
2024-07-04 17:29:28 +0200

    check permissions Downloads System

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import os
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, session, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask import send_file
+from flask import send_from_directory
+from werkzeug.utils import secure_filename
+from functools import wraps
 from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
@@ -14,12 +18,42 @@ db = SQLAlchemy(app)
 app_name = 'ClubSite'
 
 
+def check_permissions(required_permissions):
+    if 'username' not in session:
+        return False
+    session_user = db.session.get(User, session['user_id'])
+    for permission in required_permissions:
+        if not getattr(session_user, permission):
+            return False
+    return True
+
+
+def requires_permission(permission):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if not check_permissions([permission]):
+                return redirect(url_for('login'))
+            return f(*args, **kwargs)
+
+        return decorated_function
+
+    return decorator
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(100), nullable=False)
     leader = db.Column(db.Boolean, default=False)
     author = db.Column(db.Boolean, default=False)
+
+
+class Downloads(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(100), nullable=False)
+    file = db.Column(db.String(100), nullable=False)
 
 
 def create_default_user():
@@ -38,6 +72,42 @@ if not os.path.exists('instance/db.db'):
 @app.route('/')
 def index():
     return render_template('index.html', app_name=app_name)
+
+
+@app.route('/add_download', methods=['GET', 'POST'])
+@requires_permission('leader')
+def add_download():
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        file = request.files['file']
+        if file and title and description:
+            filename = secure_filename(file.filename)
+            file_path = os.path.join('/static/downloads', filename)
+            file.save(file_path)
+            new_download = Downloads(title=title, description=description, file=filename)
+            db.session.add(new_download)
+            db.session.commit()
+            flash('Download successfully added')
+            return redirect(url_for('index'))
+        else:
+            flash('Missing information')
+
+    return render_template('add_download.html')
+
+
+@app.route('/download/<int:download_id>')
+def download(download_id):
+    download = Downloads.query.get(download_id)
+    if download is None:
+        return "Download not found", 404
+    directory = '/static/downloads'
+    filename = download.file
+    file_path = os.path.join(directory, filename)
+    try:
+        return send_from_directory(directory, filename, as_attachment=True)
+    except FileNotFoundError:
+        return "File not found", 404
 
 
 @app.route('/login', methods=['GET', 'POST'])
